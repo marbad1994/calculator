cfaed5c4aad1c322ed38605eaa3f4427cf4d6e1b
marbad1994 <marcusbader@hotmail.com>
2020-08-31 22:35:12 +0200

    Adds calculator skeleton

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,28 @@
 public class Calculator {
     public static void main(String[] args ) {
-        
+        boolean isRunning = false;
+        while (isRunning) {
+
+        }
+    }
+
+    public static double addition(double x, double y) {
+        double result = 0.0;
+        return result;
+    }
+
+    public static double subtraction(double x, double y) {
+        double result = 0.0;
+        return result;
+    }
+
+    public static double multiplication(double x, double y) {
+        double result = 0.0;
+        return result;
+    }
+
+    public static double division(double x, double y) {
+        double result = 0.0;
+        return result;
     }
 }
